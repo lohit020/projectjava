1aa4400a442ea87556d44ab7f463c2ea095e3793
lohit020 <43085976+lohit020@users.noreply.github.com>
2018-09-08 14:22:16 +0530

    Add files via upload

---
+++ project/src/com/training/pos/bean/CartBean.java
@@ -0,0 +1,66 @@
+package com.training.pos.bean;
+
+import java.util.Date;
+
+public class CartBean {
+	
+	
+
+	private int cartID;
+	private String userID;
+	private String foodID;
+	private String type;
+	private int quantity;
+	private double cost;
+	private Date orderDate;
+	public int getCartID() {
+		return cartID;
+	}
+	public void setCartID(int cartID) {
+		this.cartID = cartID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getFoodID() {
+		return foodID;
+	}
+	public void setFoodID(String foodID) {
+		this.foodID = foodID;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	@Override
+	public String toString() {
+		return "CartBean [cartID=" + cartID + ", userID=" + userID + ", foodID=" + foodID + ", type=" + type
+				+ ", quantity=" + quantity + ", cost=" + cost + ", orderDate=" + orderDate + "]";
+	}
+	
+	
+
+}
---
+++ project/src/com/training/pos/bean/CredentialsBean.java
@@ -0,0 +1,41 @@
+package com.training.pos.bean;
+
+public class CredentialsBean {
+
+	private String userId;
+	private String passWord;
+	private String usertype;
+	private int loginStatus;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public String getUsertype() {
+		return usertype;
+	}
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+	public int getLoginStatus() {
+		return loginStatus;
+	}
+	public void setLoginStatus(int loginStatus) {
+		this.loginStatus = loginStatus;
+	}
+	@Override
+	public String toString() {
+		return "CredentialsBean [userId=" + userId + ", passWord=" + passWord + ", usertype=" + usertype
+				+ ", loginStatus=" + loginStatus + "]";
+	}
+	
+	
+	
+}
