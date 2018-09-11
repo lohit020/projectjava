4a135d85ce464ced1eeeb0429ee68e38c94e8c04
priyaranjan2 <prk20396@gmail.com>
2018-09-11 14:05:57 +0530

    updated

--- projectupdated/src/com/training/pos/dao/UserDaoImp.java
+++ projectupdated/src/com/training/pos/dao/UserDaoImp.java
@@ -1,7 +1,10 @@
 package com.training.pos.dao;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -18,32 +21,62 @@ public class UserDaoImp implements UserDao{
 	@Override
 	public String createUser(ProfileBean profileBean) {
 		
-	   //Write the Query to create the userprofile using crud operation
-		return null;
+	  Session session=sf.openSession();
+	  session.beginTransaction();
+	  session.save(profileBean);
+	  session.close();
+	  return "user created";
 	}
 
 	@Override
-	public int deleteUser(ArrayList<String> array) {
+	public String deleteUser(String userId) {
 		   //Write the Query to delete the userprofile using crud operation
-		return 0;
+		  Session session=sf.openSession();
+		  session.beginTransaction();
+		  Query query=session.createQuery("delete from profileBean where userId= :userId");
+		  query.setParameter("userId", userId);
+		  int result=query.executeUpdate();
+		  if(result>0)
+		  {
+			  session.close();
+			  return userId+" deleted";
+			  
+		  }
+		  else
+		  {
+			  session.close();
+			  return userId+ " not deleted";
+		  }
 	}
 
 	@Override
 	public boolean updateUser(ProfileBean profileBean) {
-		   //Write tthe Query to update th details in  the userprofile using crud operation
-		return false;
+		   //Write the Query to update the details in  the user profile using crud operation
+		 Session session=sf.openSession();
+		  session.beginTransaction();
+		  session.update(profileBean);
+		  session.close();
+		   return true;
 	}
 
 	@Override
 	public CredentialsBean findByID(String userId) {
-		   //Write the Query to locate user in the userprofile using crud operation
-		return null;
+		   //Write the Query to locate user in the user profile using crud operation
+		 Session session=sf.openSession();
+		 session.beginTransaction();
+		 CredentialsBean query=(CredentialsBean) session.createQuery("from profileBean where userId= :userId");
+		 return query;
 	}
 
 	@Override
 	public ArrayList<ProfileBean> findAll() {
-		   //Write the Query to fetch the userprofile using crud operation
-		return null;
+		   //Write the Query to fetch the user profile using crud operation
+		 Session session=sf.openSession();
+		 session.beginTransaction();
+		 Query query=session.createQuery("from ProfileBean");
+		 List<ProfileBean>profiles=query.list();
+		 return (ArrayList<ProfileBean>) profiles;
+		 
 	}
 	
 }
