118c22f0a3a2ff659a7d46a25ccb13ba984a26df
Nasreen-M <43085817+Nasreen-M@users.noreply.github.com>
2018-09-13 11:19:19 +0530

    food editing

---
+++ FoodUpdate/src/com/training/pos/bean/PosException.java
@@ -0,0 +1,11 @@
+package com.training.pos.bean;
+
+
+public class PosException extends Exception {
+	public PosException() {
+		super();
+	}
+	public PosException(String message) {
+		super(message);
+	}
+}
